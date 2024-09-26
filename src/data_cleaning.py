0e63b7c0ef5cc44e081eea75806ce17366bc3450
JuanmaLopi <juan_m.lopez_r@uao.edu.co>
2024-09-26 17:18:21 -0500

    Fill Null Values with Spotify API

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -10,12 +10,14 @@ query = "SELECT * FROM grammy_awards"
 
 # Leer los datos en un DataFrame de pandas
 grammy = pd.read_sql_query(query, conn)
-grammy
+#grammy
 
 # Reemplazar los valores nulos de Worker, por trabajador desconocido
 grammy['workers'] = grammy['workers'].fillna("Unknown Worker")
-print(grammy['workers'].isnull().sum())
+#print(grammy['workers'].isnull().sum())
 
 # Reemplazar los valor nulos de nominee, por desconocido
 grammy['nominee'] = grammy['nominee'].fillna("Unknown")
-print(grammy['nominee'].isnull().sum())
+#print(grammy['nominee'].isnull().sum())
+
+print(grammy.isnull().sum())
